179ac85603ecb2e00c1782590f3af95c227e615f
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-31 15:54:10 -0600

    init firebase app

---
+++ main.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAKwiAe0DV9z5EdUMUKQVc5weewhlQHqsg",
+  authDomain: "cool-real-time-tic-tac-toe.firebaseapp.com",
+  databaseURL: "https://cool-real-time-tic-tac-toe.firebaseio.com",
+  storageBucket: "cool-real-time-tic-tac-toe.appspot.com",
+  messagingSenderId: "408081498564"
+});
+
