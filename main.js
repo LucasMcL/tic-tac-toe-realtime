f2edeaa562684247738b89c47e1d7c1bbaf97da5
Wisbell <westleyisbell@gmail.com>
2017-02-01 10:30:09 -0600

    Created checkForWin.js file which holds the checkForWin function.  I also minimized some code in main.js and removed some unused code

--- main.js
+++ main.js
@@ -30,9 +30,7 @@ $('.cell').click(evt => {
   let position = $(evt.target).data('target')
 
   // get current player turn
-  $.when($.get(currentPlayerUrl, data => {
-    playerLetter = data
-  }))
+  $.when($.get(currentPlayerUrl, data => playerLetter = data))
     .then(data => {
 
       // Make object to patch - Lucas, I'm an idiot.  I forgot you had to make keys with variables like this
@@ -48,10 +46,17 @@ $('.cell').click(evt => {
           console.log("Patch successful?")
         }
       })
-    // Change the current players letter in firebase
+
+    // Check for wins.
+    .then(()=> {
+      checkForWin(playerLetter)
+    })
+
+    // Change the current players letter in firebase, if no one won.
     .then(data => {
       changePlayerLetter(playerLetter)
     })
+
   })
 })
 
@@ -97,11 +102,6 @@ function resetGameData() {
 
 // update cell with current players letter
 
-
-// create function to check if a player has won
-function checkForWin(){
-  console.log("checkForWin function called")
-}
 
 
 // create a function to switch letter on firebase which is called in the click event listener
