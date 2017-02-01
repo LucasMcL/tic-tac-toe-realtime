90daea47490fbee361b07e8bb38e602a48e7ecbc
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-31 20:11:47 -0600

    added comments.  removed whitespace

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ const gameStateUrl = "https://cool-real-time-tic-tac-toe.firebaseio.com/gamestat
 const gameBoardUrl = "https://cool-real-time-tic-tac-toe.firebaseio.com/gameboard.json"
 
 // add event listener on cells
+// Things that happen on click:
+	// Data at cell# changed in firebase gameboard object
+	// Player letter updated in firebase gamestate object
+
 $('.cell').click(evt => {
 
   let playerLetter;
@@ -20,30 +24,29 @@ $('.cell').click(evt => {
   let position = $(evt.target).data('target')
 
   // get current player turn
-    $.when($.get(currentPlayerUrl, data => {
-      playerLetter = data
-    }))
-      .then(data => {
+  $.when($.get(currentPlayerUrl, data => {
+    playerLetter = data
+  }))
+    .then(data => {
 
-        // Make object to patch - Lucas, I'm an idiot.  I forgot you had to make keys with variables like this
-        let changeLetterPatch = {}
-        changeLetterPatch[position] = playerLetter;
+      // Make object to patch - Lucas, I'm an idiot.  I forgot you had to make keys with variables like this
+      let changeLetterPatch = {}
+      changeLetterPatch[position] = playerLetter;
 
-        // Make change on gameboard in firebase
-        $.ajax({
-          url: gameBoardUrl,
-          type: 'PATCH',
-          data: JSON.stringify(changeLetterPatch),
-          success: function(response) {
-            console.log("Patch successful?")
-          }
-        })
-
-      // Change the current players letter in firebase
-      .then(data => {
-        changePlayerLetter(playerLetter)
+      // Make change on gameboard in firebase
+      $.ajax({
+        url: gameBoardUrl,
+        type: 'PATCH',
+        data: JSON.stringify(changeLetterPatch),
+        success: function(response) {
+          console.log("Patch successful?")
+        }
       })
+    // Change the current players letter in firebase
+    .then(data => {
+      changePlayerLetter(playerLetter)
     })
+  })
 
   // update firebase with "X" or "O" at selected position in the game?
 
@@ -80,26 +83,21 @@ function resetGameBoard(){
 }
 
 
-
-
 // create function to check if a player has won
 function checkForWin(){
   console.log("checkForWin function called")
 }
 
 
-
-
 // create a function to switch letter on firebase which is called in the click event listener
-  // I know I repeated myself
+// I know I repeated myself
+// Looks fine to me! - Lucas
 function changePlayerLetter(currentPlayerLetter){
   console.log("changePlayerLetter function called")
 
   if(currentPlayerLetter === "X") {
     console.log("New letter is O")
-    let newLetter = {
-                      "current_player": "O"
-                    }
+    let newLetter = { "current_player": "O" }
 
     $.ajax({
           url: gameStateUrl,
@@ -111,9 +109,7 @@ function changePlayerLetter(currentPlayerLetter){
         })
   } else if (currentPlayerLetter === "O") {
     console.log("New letter is X")
-    let newLetter = {
-                      "current_player": "X"
-                    }
+    let newLetter = { "current_player": "X" }
 
     $.ajax({
           url: gameStateUrl,
@@ -126,6 +122,4 @@ function changePlayerLetter(currentPlayerLetter){
   } else {
     console.log("The current_player was neither 'X' or 'O' ")
   }
-
-
 }
