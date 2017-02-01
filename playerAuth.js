683f78f3d0194f99ca14008c51f4be8aa53536f9
LucasMcL <LMcLaughlin09@gmail.com>
2017-02-01 17:29:04 -0600

    add comments

--- playerAuth.js
+++ playerAuth.js
@@ -1,3 +1,7 @@
+// To do: deal with user refreshing page, or leaving and coming back
+	// problem: user object is deleted upon disconnect
+	// user object is not recreated upon reconnection
+
 console.log('playerAuth.js loaded')
 
 // Event listeners
@@ -7,6 +11,7 @@ firebase.auth().onAuthStateChanged((user) => {
 	if(user) {
 		// Hide modal add soon as user successfully made
 		$('#sign-in-modal').modal('hide')
+		console.log('user added in firebase')
 	}
 	else {
 		// If no user signed in
@@ -22,16 +27,24 @@ function createUser(submitEvt) {
 	submitEvt.preventDefault()
 
 	const display_name = $('#sign-in-modal input[type="text"]').val()
+	let uid
+
 	firebase.auth().signInAnonymously()
 		.then(user => {
-			const uid = user.uid
+			uid = user.uid
 			activeUsersRef.push({ display_name, uid })
+				.then((snap) => {
+					console.log('user added in our database')
+					const post_id = snap.key
+					const userRef = firebase.database().ref(`activeUsers/${post_id}`)
+					userRef.onDisconnect().remove()
+				})
 		})
 }
 
 // Reference this later
 
-// var presenceRef = firebase.database().ref("disconnectmessage");
+// var userRef = firebase.database().ref("disconnectmessage");
 // // Write a string when this client loses connection
 // presenceRef.onDisconnect().set("I disconnected!");
 
